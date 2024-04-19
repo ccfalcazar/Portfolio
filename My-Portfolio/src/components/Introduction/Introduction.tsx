a5b03f44563c775b4f4b1adbb1bc2c52ee30d8e2
Christian <ccfalcazar@gmail.com>
2024-04-19 08:25:50 +0800

    Update

--- My-Portfolio/src/components/Introduction/Introduction.tsx
+++ My-Portfolio/src/components/Introduction/Introduction.tsx
@@ -4,7 +4,7 @@ function Introduction()
 {
     return (
         <>
-        <p id="intro" className="display-3 text-justify text-dark">Hi! I'm</p>
+        <p id="intro" className="text-justify text-dark">Hi! I'm</p>
         <p id="name"className="display-1 fw-bold">Christian Alcazar</p>
         <p id="position" className="text-primary fst-italic fw-bold display-5">Web and Application Developer</p> 
         <p id="sub-intro" className="display-6 text-justify text-black-50">experienced in both manual and automated testing.</p>
