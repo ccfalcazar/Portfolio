d4465cf56262446377afc8e11eeb909a419b09dd
Christian <ccfalcazar@gmail.com>
2024-04-21 18:11:49 +0800

    update src folder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+
+function Header()
+{
+    return(
+    <p className="text-center text-dark h2">Hi, My name is <span className="h1 fw-bold">Christian Carlo Alcazar</span></p>
+    );
+}
+
+export default Header;
---
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -0,0 +1,11 @@
+
+function LeftPanel()
+{
+    return (
+    <div className="vh-100 d-flex align-item-center">
+        <h1 className="">Left Side</h1>
+    </div>
+    );
+}
+
+export default LeftPanel;
