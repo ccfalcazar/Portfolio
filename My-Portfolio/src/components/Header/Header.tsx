7132b7f185d028cd431003c3939ab516d62ebe19
Christian <ccfalcazar@gmail.com>
2024-04-21 18:11:10 +0800

    Initial Commit

--- My-Portfolio/src/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-
-function Header()
-{
-    return(
-    <p className="text-center text-dark h2">Hi, My name is <span className="h1 fw-bold">Christian Carlo Alcazar</span></p>
-    );
-}
-
-export default Header;
