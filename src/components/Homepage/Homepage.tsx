6ba8e2783cd8d921c4a4c739d9cf76833cab298c
Christian <ccfalcazar@gmail.com>
2024-04-22 00:21:50 +0800

    Housekeeping
    Removed unused components and added an AboutMe component

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -5,12 +5,12 @@ function Homepage()
 {
     return (
             <div id="Homepage" className='p-0 m-0 vh-100 d-flex'>
-                <div id="card-container" className='d-flex p-0 m-0 w-50 m-auto border border-5 border-white rounded-5 shadow'>
+                <div className='d-none d-md-block p-0 m-0 w-50 m-auto border border-5 border-white rounded-5 shadow'>
                     <div className='w-50 p-5 mt-3 mb-3 border-end border-3 border-white'>
                     <Introduction/>
                     </div>
                     <div className='w-50'>
-                        <h5>Projects</h5>
+                        
                     </div>
                 </div>
             </div>
