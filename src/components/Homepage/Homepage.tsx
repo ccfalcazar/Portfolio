c36a2c97f798207ed5db76e7a34734e845e2fb32
Christian <ccfalcazar@gmail.com>
2024-05-23 22:44:04 +0800

    UpdatePortfolioLaout

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -6,12 +6,9 @@ function Homepage()
 {
     return (
             <div id="Homepage" className='p-0 m-0'>
-                <div id='FixedSide' className='d-none d-lg-block m-auto'>
-                    <Introduction/>
-                </div>
-                <div className='row'>
-                    <div className='col-md-6 pt-5 px-4 ms-lg-auto me-3'>
-                        <AboutMe/>
+                <div className='row vh-100 p-4'>
+                    <div className='col-md-6 align-self-center mx-auto my-auto'>
+                        <Introduction/>
                     </div>
                 </div>
             </div>
--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -4,10 +4,9 @@ function Introduction()
 {
     return (
         <>
-        <h3 id="intro" className="text-justify text-dark">Hi! I'm</h3>
-        <h1 id="name"className="fw-bold">Christian Alcazar</h1>
-        <h2 id="position" className="text-primary fst-italic fw-bold">Web and Application Developer</h2> 
-        <h3 id="sub-intro" className="text-justify text-black lead">experienced in both manual and automated testing.</h3>
+        <h1 id="name"className="fw-bold display-2 lead">Christian Alcazar</h1>
+        <h2 id="position" className="text-primary display-6 fst-italic fw-bold">Web and Application Developer</h2> 
+        <h4 id="sub-intro" className="text-justify text-black font-monospace text-black-50">experienced in both manual and automated testing.</h4>
         </>
     );
 }
