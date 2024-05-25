c827de379f33b6657815ce3267e4724e1396d8bc
Christian <ccfalcazar@gmail.com>
2024-05-25 22:57:55 +0800

    Update About

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -2,15 +2,54 @@ import '../AboutMe/AboutMe.css';
 
 function AboutMe()
 {
+
+    function Star(rating: number)
+    {
+        if(rating == 1)
+            return <span className="text-warning bi bi-star-fill mx-1" />
+        else
+        return  (<span className="text-warning bi bi-star-fill mx-1">{Star(rating-1)}</span>);
+    }
+
+    function SkillList(Skill:string, rating: number)
+    {
+        return(
+        <li className='list-group-item'>
+        <div className='row m-0 p-0'>
+            <div className='col p-0 m-0'>{Skill}</div>
+            <div className='col m-0 p-0'>
+                {Star(rating)}
+            </div>
+        </div>
+        </li>
+        )
+    }
+
     return (
     <>
-        <div className='w-75 m-auto'>
-            <div className='border border-primary p-3'>
-                <p className='header display-2 text-black-50'>About Me</p>
-                <ul className='list-group list-group-flush '>
-                <li className='display-6 list-group-item bg-transparent border-dark'>Learned Turbo C++, Visual Basic, VBA, Java in highschool as a Computer Technology Student</li>
-                <li className='display-6 list-group-item bg-transparent'>Learned Turbo C++, Visual Basic, VBA, Java in highschool as a Computer Technology Student</li>
-                </ul>
+        <div className='m-auto'>
+            <div className='row'>
+                <div className='p-3 col-md-6'>
+                    <h2 className='header text-primary'>Programming Language</h2>
+                    <ul className='list-group'>
+                    {SkillList("C++", 5)}
+                    {SkillList("C#", 5)}
+                    {SkillList("Visual Basic", 3)}
+                    {SkillList("VBA", 3)}
+                    {SkillList("Java", 2)}
+                    </ul>
+                </div>
+                <div className='p-3 col-md-6'>
+                    <h2 className='header text-primary'>Web Development</h2>
+                    <ul className='list-group'>
+                        {SkillList("HTML and CSS", 5)}
+                        {SkillList("Bootstrap", 5)}
+                        {SkillList("Javascript", 5)}
+                        {SkillList("PHP", 4)}
+                        {SkillList("ReactJS", 4)}
+                        {SkillList("Asp.Net", 3)}
+                    </ul>
+                </div>
             </div>
         </div>
     </>
--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -11,22 +11,22 @@ function Introduction()
     return (
         <>
         <div className='m-auto w-75'>
-            <div>
-                    <h1 id="name"className="fw-bold display-1 text-dark">Christian Alcazar</h1>
-                    <h3 id="position" className="text-primary fw-bold">WEB AND APPLICATION DEVELOPER</h3> 
-                    <p id="sub-intro" className="fs-6 text-justify text-black font-monospace fst-italic text-black-50">
-                        I am a versatile Computer Engineer with knowledge in 
-                        <a className='link-dark link-opacity-75 link-underline link-underline-opacity-0' href='https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/sockets/sockets-overview'> Socket Programming</a>,
-                        <a className='link-dark link-opacity-75 link-underline link-underline-opacity-0' href='https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/'> Asynchronous Programming</a> and 
-                        <a className='link-dark link-opacity-75 link-underline link-underline-opacity-0' href='https://learn.microsoft.com/en-us/dotnet/api/system.io.ports.serialport?view=net-8.0'> Microcontroller Interfacing</a>.
-                        Adding what I've learned as an Associate Quality Assurance Engineer, I can create a more modular, flexible, sustainable and maintainable programs for fast and easy testing.
-                    </p>
-                <div id='shortcut' className='mt-3 m-0'>
-                        <span className='btn btn-sm btn-outline-primary rounded me-1' onClick={()=>handlesClick('AboutMe')}>About</span>
-                        <span className='btn btn-sm btn-outline-primary rounded mx-1'>Experiences</span>
-                        <span className='btn btn-sm btn-outline-primary rounded mx-1'>Projects</span>
-                </div>
-             </div>
+            <div className='p-0 m-0'>
+                <h1 id="name"className="fw-bold display-1 text-dark">Christian Alcazar</h1>
+                <h3 id="position" className="text-primary fw-bold">WEB AND APPLICATION DEVELOPER</h3> 
+                <p id="sub-intro" className="fs-6 text-justify text-black font-monospace fst-italic text-black-50">
+                    I am a versatile Computer Engineer with knowledge in 
+                    <a className='link-dark link-opacity-75 link-underline link-underline-opacity-0' href='https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/sockets/sockets-overview'> Socket Programming</a>,
+                    <a className='link-dark link-opacity-75 link-underline link-underline-opacity-0' href='https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/'> Asynchronous Programming</a> and 
+                    <a className='link-dark link-opacity-75 link-underline link-underline-opacity-0' href='https://learn.microsoft.com/en-us/dotnet/api/system.io.ports.serialport?view=net-8.0'> Microcontroller Interfacing</a>.
+                    Adding what I've learned as an Associate Quality Assurance Engineer, I can create a more modular, flexible, sustainable and maintainable programs for fast and easy testing.
+                </p>
+            </div>
+            <div id='shortcut' className='mt-3 m-0'>
+                <span className='btn btn-sm btn-outline-primary rounded me-1' onClick={()=>handlesClick('AboutMe')}>About</span>
+                <span className='btn btn-sm btn-outline-primary rounded mx-1'>Experiences</span>
+                <span className='btn btn-sm btn-outline-primary rounded mx-1'>Projects</span>
+            </div>
         </div>
         </>
     );
